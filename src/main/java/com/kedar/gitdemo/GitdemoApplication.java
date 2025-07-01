c14e84a531cd8acb7b00cc4dd36d38cd2ec086a1
Kedarinathchowdary Mullaguri <kedar@Mac.attlocal.net>
2025-07-01 14:40:06 -0500

    feature 1 bug fix

--- src/main/java/com/kedar/gitdemo/GitdemoApplication.java
+++ src/main/java/com/kedar/gitdemo/GitdemoApplication.java
@@ -2,7 +2,7 @@ package com.kedar.gitdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// test feature 1
+// test feature 1, feature 1 bug fix
 @SpringBootApplication
 public class GitdemoApplication {
 
@@ -11,3 +11,4 @@ public class GitdemoApplication {
 	}
 
 }
+
